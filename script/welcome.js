e753cf72581bed701211209a8f8a36917a6e58de
brendonwai <brendonwai@me.com>
2017-11-13 17:55:38 -0800

    Bootstrap, Greensock, and ScrollMagic

--- script/welcome.js
+++ script/welcome.js
@@ -1,14 +1,18 @@
 $(document).ready(function(e) {
+  TweenLite.to($('.terminal'), 0.6, {opacity: 1, onComplete: terminalType});
+});
+
+function terminalType() {
   $('#typedText').typeIt({
     speed: 50,
     autoStart: false,
     lifeLike: true
   })
-  .tiType(Prism.highlight('class Programmer {',Prism.languages.cpp))
+  .tiType(Prism.highlight('class Software_Engineer {',Prism.languages.cpp))
   .tiBreak().tiPause(500)
   .tiType(Prism.highlight('public: ',Prism.languages.cpp)).tiBreak().tiPause(300)
   .tiType(Prism.highlight('std::string name = "Brendon Wai";',Prism.languages.cpp))
   .tiBreak().tiPause(500)
   .tiType(Prism.highlight('std::string message = "Welcome to my homepage!";',Prism.languages.cpp))
   .tiBreak().tiType('}');
-});
+};
